8ea05e04a44359d4060c83051787476f7dd99f48
tangledpuzzle <haroldpatton751@gmail.com>
2015-12-02 12:15:26 +0000

    Closes #1. Added dummy reviews and a init.sql to fully initialize the database

---
+++ dev/init.sql
@@ -0,0 +1,4 @@
+source ~/workspace/r2projekti/dev/database.sql;
+source ~/workspace/r2projekti/dev/users.sql;
+source ~/workspace/r2projekti/dev/products.sql;
+source ~/workspace/r2projekti/dev/reviews.sql;

91f5b9b177bc982a0235f0fca16ed9b2f2ef4b94
tangledpuzzle <haroldpatton751@gmail.com>
2015-12-03 13:08:43 +0000

    Implemented HTTP POST queries in the REST API

--- application/controllers/api/Requests.php
+++ application/controllers/api/Requests.php
@@ -18,6 +18,9 @@ class Requests extends REST_Controller
 		$this->methods['user_delete']['limit'] = 50; // 50 requests per hour per user/key
 	}
 
+	/*
+	 * HTTP GET: USERS
+	 */
 	public function users_get()
 	{
 		// Get userid parameter from the query.
@@ -27,7 +30,7 @@ class Requests extends REST_Controller
 		if ($userid === NULL)
 		{
 			// TODO: Get all users from the database.
-			$users = 'dummy get all users';
+			$users = ['dummydata' => 'dummy get all users'];
 			
 			// TODO: Check if the users data store contains users (in case the database result returns NULL)
 			if ($users)
@@ -36,7 +39,7 @@ class Requests extends REST_Controller
 			}
 			else
 			{
-				$this->response("No users found.", REST_Controller::HTTP_NOT_FOUND);
+				$this->response(['status' => FALSE, 'message' => 'No users were found'], REST_Controller::HTTP_NOT_FOUND);
 			}
 		}
 
@@ -62,10 +65,13 @@ class Requests extends REST_Controller
 		}
 		else
 		{
-			$this->set_response("User with the ID " . $userid . " not found.", REST_Controller::HTTP_NOT_FOUND);
+			$this->set_response(['status' => FALSE, 'message' => "User with the ID " . $userid . " not found."], REST_Controller::HTTP_NOT_FOUND);
 		}
 	}
 	
+	/*
+	 * HTTP GET: USER DATA
+	 */
 	public function userdata_get()
 	{
 		// Get userid parameter from the query.
@@ -93,88 +99,340 @@ class Requests extends REST_Controller
 
 		if (empty($user))
 		{
-			$this->set_response("User with the ID " . $userid . " not found.", REST_Controller::HTTP_NOT_FOUND);
+			$this->set_response(['status' => FALSE, 'message' => "User with the ID " . $userid . " not found."], REST_Controller::HTTP_NOT_FOUND);
 		}
-
-		if ($datatype !== NULL)
+		else
 		{
-			// Error checking, this should always be true when datatype is not NULL.
-			if ($collectionid === NULL && $commentid === NULL)
+			if ($datatype !== NULL)
 			{
-				switch ($datatype)
+				// Error checking, this should always be true when datatype is not NULL.
+				if ($collectionid === NULL && $commentid === NULL)
 				{
-					case "collections":
-						$collections = "dummy get all user collections";
-			
-						if ($collections)
-						{
-							$this->response($collections, REST_Controller::HTTP_OK);
-						}
-						else
-						{
-							$this->response("User has no collections.", REST_Controller::HTTP_NOT_FOUND);
-						}
-						break;
-					case "comments":
-						$comments = "dummy get all user comments";
-			
-						if ($comments)
-						{
-							$this->response($comments, REST_Controller::HTTP_OK);
-						}
-						else
-						{
-							$this->response("User has no comments.", REST_Controller::HTTP_NOT_FOUND);
-						}
-						break;
-					default:
-						// Theoretically this is not possible.
-						$this->response(NULL, REST_Controller::HTTP_BAD_REQUEST);
-				}
+					switch ($datatype)
+					{
+						case "collections":
+							$collections = ['dummydata' => "dummy get all user collections"];
 				
-				$this->response(NULL, REST_Controller::HTTP_BAD_REQUEST);
+							if ($collections)
+							{
+								$this->response($collections, REST_Controller::HTTP_OK);
+							}
+							else
+							{
+								$this->response(['status' => FALSE, 'message' => "User has no collections."], REST_Controller::HTTP_NOT_FOUND);
+							}
+							break;
+						case "comments":
+							$comments = ['dummydata' => "dummy get all user comments"];
+				
+							if ($comments)
+							{
+								$this->response($comments, REST_Controller::HTTP_OK);
+							}
+							else
+							{
+								$this->response(['status' => FALSE, 'message' => "User has no comments."], REST_Controller::HTTP_NOT_FOUND);
+							}
+							break;
+						default:
+							// Theoretically this is not possible.
+							$this->response(NULL, REST_Controller::HTTP_BAD_REQUEST);
+					}
+					
+					$this->response(NULL, REST_Controller::HTTP_BAD_REQUEST);
+				}
+			}
+
+			// Determine what data to get.
+			if ($collectionid === NULL)
+			{
+				$commentid = (int) $commentid;
+				$collectionid = 0; // Invalid ID
+			}
+			else
+			{
+				$collectionid = (int) $collectionid;
+				$commentid = 0; // Invalid ID
+			}
+			
+			// One of these is 0 the other must not be 0 or less.
+			if ($collectionid <= 0 && $commentid <= 0)
+			{
+					$this->response(NULL, REST_Controller::HTTP_BAD_REQUEST);
+			}
+			else if ($collectionid > $commentid) // One of these values is greater than 0.
+			{
+				// TODO: Get specific collection from database.
+				$data = NULL;
+				// Error message to return if the comment does not exist.
+				$errorMessage = "User ID " . $userid . " does not have a collection with the ID " . $collectionid . ".";
+			}
+			else
+			{
+				// TODO: Get specific comment from database.
+				$data = NULL;
+				// Error message to return if the comment does not exist.
+				$errorMessage = "User ID " . $userid . " does not have a comment with the ID " . $commentid . ".";
+			}
+				
+			if (!empty($data))
+			{
+				$this->set_response($data, REST_Controller::HTTP_OK);
+			}
+			else
+			{
+				$this->set_response(['status' => FALSE, 'message' => $errorMessage], REST_Controller::HTTP_NOT_FOUND);
+			}
+		}
+	}
+	
+	/*
+	 * HTTP GET: PRODUCTS
+	 */
+	public function products_get()
+	{
+		// Get productid parameter from the query.
+		$productid = $this->get('productid');
+		
+		// If the productid is NULL, return all products.
+		if ($productid === NULL)
+		{
+			// TODO: Get all products from the database.
+			$products = ['dummydata' => 'dummy get all products'];
+			
+			// TODO: Check if the products data store contains products (in case the database result returns NULL)
+			if ($products)
+			{
+				$this->response($products, REST_Controller::HTTP_OK);
+			}
+			else
+			{
+				$this->response(['status' => FALSE, 'message' => 'No products were found'], REST_Controller::HTTP_NOT_FOUND);
 			}
 		}
 
-		// Determine what data to get.
-		if ($collectionid === NULL)
+		// TODO: Find and return a single record for a particular product.
+		$productid = (int) $productid;
+
+		/* Validate the ID.
+		   ProductID field in the database must be >= 1.
+		   TODO: Move to separate function later.
+		 */
+		if ($productid <= 0)
 		{
-			$commentid = (int) $commentid;
-			$collectionid = 0; // Invalid ID
+			// Invalid id.
+			$this->response(NULL, REST_Controller::HTTP_BAD_REQUEST);
+		}
+
+		// TODO: Get specific product from database.
+		$product = NULL;
+
+		if (!empty($product))
+		{
+			$this->set_response($product, REST_Controller::HTTP_OK);
 		}
 		else
 		{
-			$collectionid = (int) $collectionid;
-			$commentid = 0; // Invalid ID
+			$this->set_response(['status' => FALSE, 'message' => "Product with the ID " . $productid . " not found."], REST_Controller::HTTP_NOT_FOUND);
 		}
+	}
+	
+	/*
+	 * HTTP GET: PRODUCT DATA
+	 */
+	public function productdata_get()
+	{
+		// Get productid parameter from the query.
+		$productid = $this->get('productid');
+	
+		// Get reviewid parameter from the query.
+		$reviewid = $this->get('reviewid');
 		
-		// One of these is 0 the other must not be 0 or less.
-		if ($collectionid <= 0 && $commentid <= 0)
+		// Get commentid parameter from the query.
+		$commentid = $this->get('commentid');
+		
+		// Get datatype parameter from the query.
+		$datatype = $this->get('datatype');
+		
+		// TODO: Validate productid in a separate function.
+		if ($productid === NULL || (int) $productid <= 0)
 		{
-				$this->response(NULL, REST_Controller::HTTP_BAD_REQUEST);
+			$this->response(NULL, REST_Controller::HTTP_BAD_REQUEST);
 		}
-		else if ($collectionid > $commentid) // One of these values is greater than 0.
+
+		$productid = (int) $productid;
+
+		// TODO: ID validation function
+		$product = NULL;
+
+		if (empty($product))
 		{
-			// TODO: Get specific collection from database.
-			$data = NULL;
-			// Error message to return if the comment does not exist.
-			$errorMessage = "User ID " . $userid . " does not have a collection with the ID " . $collectionid . ".";
+			$this->set_response(['status' => FALSE, 'message' => "Product with the ID " . $productid . " not found."], REST_Controller::HTTP_NOT_FOUND);
 		}
 		else
 		{
-			// TODO: Get specific comment from database.
-			$data = NULL;
-			// Error message to return if the comment does not exist.
-			$errorMessage = "User ID " . $userid . " does not have a comment with the ID " . $commentid . ".";
-		}
+			if ($datatype !== NULL)
+			{
+				// Valid cases:
+				// - Both reviewid and commentid are null -> get all data of specified type
+				// - reviewid is not null and commentid is null (handled further down)
+				if ($reviewid === NULL && $commentid === NULL)
+				{
+					switch ($datatype)
+					{
+						case "reviews":
+							$reviews = ['dummydata' => "dummy get all product reviews"];
+				
+							if ($reviews)
+							{
+								$this->response($reviews, REST_Controller::HTTP_OK);
+							}
+							else
+							{
+								$this->response(['status' => FALSE, 'message' => "Product ID " . $productid . " has no reviews."], REST_Controller::HTTP_NOT_FOUND);
+							}
+							break;
+						case "comments":
+							$comments = ['dummydata' => "dummy get all product comments"];
+				
+							if ($comments)
+							{
+								$this->response($comments, REST_Controller::HTTP_OK);
+							}
+							else
+							{
+								$this->response(['status' => FALSE, 'message' => "Product ID " . $productid . " has no comments."], REST_Controller::HTTP_NOT_FOUND);
+							}
+							break;
+						default:
+							// Theoretically this is not possible.
+							$this->response(NULL, REST_Controller::HTTP_BAD_REQUEST);
+					
+						$this->response(NULL, REST_Controller::HTTP_BAD_REQUEST);
+					}
+				}
+			}
 			
-		if (!empty($data))
+			$both = FALSE;
+			
+			// Determine what data to get.
+			if ($reviewid === NULL)
+			{
+				$commentid = (int) $commentid;
+				$reviewid = 0; // Invalid ID
+			}
+			else if ($commentid === NULL)
+			{
+				$reviewid = (int) $reviewid;
+				$commentid = 0; // Invalid ID
+				
+				// Get all review comments
+				if ($datatype === "comments")
+				{
+					$comments = ['dummydata' => "dummy get all product review comments"];
+				
+					if ($comments)
+					{
+						$this->response($comments, REST_Controller::HTTP_OK);
+					}
+					else
+					{
+						$this->response(['status' => FALSE, 'message' => "Product ID " . $productid . " review ID " . $reviewid. " has no comments."], REST_Controller::HTTP_NOT_FOUND);
+					}
+				}
+			}
+			else
+			{
+				// Neither are NULL, get specific comment from specific review.
+				$commentid = (int) $commentid;
+				$reviewid = (int) $reviewid;
+				$both = TRUE;
+			}
+			
+			if ($reviewid <= 0 && $commentid <= 0)
+			{
+					$this->response(NULL, REST_Controller::HTTP_BAD_REQUEST);
+			}
+			else if (!$both && ($reviewid > $commentid)) // One of these ID values is greater than 0.
+			{
+				// TODO: Get specific review from database.
+				$data = NULL;
+				// Error message to return if the comment does not exist.
+				$errorMessage = "Product ID " . $productid . " does not have a review with the ID " . $reviewid . ".";
+			}
+			else if (!$both)
+			{
+				// TODO: Get specific comment from database.
+				$data = NULL;
+				// Error message to return if the comment does not exist.
+				$errorMessage = "Product ID " . $productid . " does not have a comment with the ID " . $commentid . ".";
+			}
+			else // both is TRUE.
+			{
+				// TODO: Get specific comment from specific review.
+				// TODO: Validate review id.
+				$data = NULL;
+				// Error message to return if the comment does not exist.
+				$errorMessage = "Product ID " . $productid . " review ID " . $reviewid . " does not have a comment with the ID " . $commentid . ".";
+			}
+			
+			if (!empty($data))
+			{
+				$this->set_response($data, REST_Controller::HTTP_OK);
+			}
+			else
+			{
+				$this->set_response(['status' => FALSE, 'message' => $errorMessage], REST_Controller::HTTP_NOT_FOUND);
+			}
+		}
+	}
+
+	/*
+	 * HTTP POST: COMMENTS
+	 */
+	public function comments_post()
+	{
+		// Get productid parameter from the query.
+		$productid = $this->get('productid');
+	
+		// Get reviewid parameter from the query.
+		$reviewid = $this->get('reviewid');
+		
+		// Get userid parameter from the query.
+		$userid = $this->get('userid');
+		
+		// Get comment text.
+		$text = $this->post('text');
+		
+		if ($productid === NULL)
 		{
-			$this->set_response($data, REST_Controller::HTTP_OK);
+			// User comment.
+			if ($userid !== NULL)
+			{
+				$userid = (int) $userid;
+
+				// TODO: Validate userid.
+				$this->set_response(['status' => TRUE, 'message' => "Comment posted to user ID " . $userid . ": " . $text], REST_Controller::HTTP_OK);
+			}
+			else
+			{
+				$this->response(NULL, REST_Controller::HTTP_BAD_REQUEST);
+			}
 		}
 		else
 		{
-			$this->set_response($errorMessage, REST_Controller::HTTP_NOT_FOUND);
+			// Review comment.
+			if ($reviewid !== NULL)
+			{
+				$reviewid = (int) $reviewid;
+
+				// TODO: Validate reviewid.
+				$this->set_response(['status' => TRUE, 'message' => "Comment posted to review ID " . $reviewid . " on product ID " . $productid . "."], REST_Controller::HTTP_OK);
+			}
+			else
+			{
+				$this->response(NULL, REST_Controller::HTTP_BAD_REQUEST);
+			}
 		}
 	}
 }

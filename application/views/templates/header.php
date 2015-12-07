67445494b40aad577f785b5ae0a53d23155e0be1
tangledpuzzle <haroldpatton751@gmail.com>
2015-12-07 12:05:48 +0000

    footer and header edit

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,12 +5,42 @@
 
 </head>
 <body>
+<style>
+	
+	#a{
+		  position: absolute;
+    top: 120px;
+    right: 540px;
+    width: 100px
+	}
+	
+	#b{
+		  position: absolute;
+    top: 120px;
+    left: 540px;
+    width: 100px
+	}
+
+	.header {
+
+width: 940px;
+height: 100px;
+margin: 5px auto;
+border: 1px solid black;
+text-align: center;
+background-color: blue;
+
+}
+	</style>
 
 <div class="header"><h1>R2Projekti</h1></div>
 	<!-- login form -->
-		<form action="http://www.frmtr.com/login.php?do=login" method="post" onsubmit="md5hash(vb_login_password, vb_login_md5password, vb_login_md5password_utf, 0)">
+
+	<form id="a" action="http://www.frmtr.com/login.php?do=login" method="post" onsubmit="md5hash(vb_login_password, vb_login_md5password, vb_login_md5password_utf, 0)">
 		<script type="text/javascript" src="clientscript/vbulletin_md5.js?v=389"></script>
+			
 		<table cellpadding="2px" cellspacing="3" border="0" style="float:right">
+			
 		<tr>
 
 			<td><input type="text" class="bginput" style="font-size: 11px" name="vb_login_username" id="navbar_username" size="10" accesskey="u" tabindex="101" value="Username" onfocus="if (this.value == 'username') this.value = '';" /></td>
@@ -22,15 +52,19 @@
 
 		</tr>
 		</table>
+		
 		<input type="hidden" name="s" value="9a29479bd38ed059faea109e6966b521" />
 		<input type="hidden" name="securitytoken" value="guest" />
 		<input type="hidden" name="do" value="login" />
 		<input type="hidden" name="vb_login_md5password" />
 		<input type="hidden" name="vb_login_md5password_utf" />
 		</form>
+
 		<!-- / login form -->
- <form accept-charset="UTF-8" action="https://www.verkkokauppa.com/fi/search" id="header-search" method="get">
-                    <fieldset>
+
+	 
+	<form id="b" accept-charset="UTF-8" action="https://www.verkkokauppa.com/fi/search" id="header-search" method="get">
+                   
                         <legend>Keywords</legend>
                         <label>
                             <input autocomplete="off" placeholder="give a word or item id" name="q" tabindex="1" type="text" required title="" />
@@ -40,6 +74,6 @@
                             <span>Search</span>
                         </label>
                         <noscript><input type="hidden" value="1" name="noscript" /></noscript>
-                    </fieldset>
+                  
 </form>
 

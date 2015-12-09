b4a9592bd747496a6220133f4defc6478c98dce9
tangledpuzzle <haroldpatton751@gmail.com>
2015-12-09 13:53:35 +0000

    productlist valmis kai

--- application/views/pages/productlist.php
+++ application/views/pages/productlist.php
@@ -2,6 +2,8 @@
 
 <h1>Book List</h1>
 
+<!--<link rel="stylesheet" type="text/css" href="<?php echo base_url('css/bsdev.css'); ?>">-->
+
 <div id="productlist"></div>
 
 <?php echo '<script type="text/javascript"> var json = ' . json_encode($this->r2pdb_model->get_products_display()) . ';</script>' ;?>
---
+++ js/productview.js
@@ -0,0 +1,10 @@
+/**
+* Created with webprojekti.
+* User: ME-varjoil
+* Date: 2015-12-09
+* Time: 01:51 PM
+* To change this template use Tools | Templates.
+*/
+
+
+document.getElementById("productview").appendChild(prodlist_productdiv);

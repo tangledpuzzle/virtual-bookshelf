939cf2f870caeac7c1ad71f2b4ece52b569e5975
tangledpuzzle <haroldpatton751@gmail.com>
2015-12-10 15:30:13 +0000

    frontpage

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,19 +1,11 @@
 
+		</div>
+
+		<div class="row">
+			<div class=" navbar-inverse bottom-align">
+				<div class="vcenter" >Powered by Ryhmä 2</div>
+			</div>
+		</div>
 	</div>
-</div>
-
-<div class=" navbar-inverse">
-
-
-
-   <div class="vcenter" >Powered by Ryhmä 2</div>
-	 
-  </div>
-	</div>
-
-
-
-
-</div>
 </body>
 </html>

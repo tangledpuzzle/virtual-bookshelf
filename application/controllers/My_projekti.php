35b7b0efe2b9b9557f9eacdc3a43747a2a5cc6ce
tangledpuzzle <haroldpatton751@gmail.com>
2015-12-12 17:42:12 +0000

    Removed temporary debug code

--- application/controllers/My_projekti.php
+++ application/controllers/My_projekti.php
@@ -9,36 +9,22 @@ class My_projekti extends MY_Controller
 	
 	public function view($page = 'front', $data = NULL)
 	{
-		if ( ! file_exists(APPPATH . 'views/pages/' . $page . '.php'))
+		if (!file_exists(APPPATH . 'views/pages/' . $page . '.php'))
 		{
-			echo "Can't find " . APPPATH . 'views/pages/' . $page . '.php';
+			echo "Unable to find " . APPPATH . 'views/pages/' . $page . '.php';
 			show_404();
 		}
 		
+		// Some Community Auth magic that is required for reading the cookie.
 		$this->is_logged_in();
 
-		if( $http_user_cookie_contents = $this->input->cookie( config_item('http_user_cookie_name') ) )
-            {
-                $data["cookie"] = unserialize( $http_user_cookie_contents );
-            }
-		else
+		if( $http_user_cookie_contents = $this->input->cookie(config_item('http_user_cookie_name')))
 		{
-			$data['cookie'] = "lols";
+			$data["cookie"] = unserialize( $http_user_cookie_contents );
 		}
-        if( ! empty( $this->auth_role ) )
-        {
-            echo 'User ID is ' . $this->auth_user_id;
 			
 		$this->load->view('templates/header', $data);
 		$this->load->view('pages/'.$page, $data);
 		$this->load->view('templates/footer');
-		}
-		else
-		{
-				
-		$this->load->view('templates/header');
-		$this->load->view('pages/'.$page, $data);
-		$this->load->view('templates/footer');
-			}
 	}
 }

3e042a004445f3d1366aa2474eaf6ac38394c141
tangledpuzzle <haroldpatton751@gmail.com>
2015-12-15 20:19:50 +0000

    Fixes #49

--- application/views/pages/error.php
+++ application/views/pages/error.php
@@ -1,11 +1,18 @@
-<?php 
-if (isset($header))
+<?php
+if (isset($error_message))
 {
-	echo '<h3 class="first-content-element">' . $header . '</h3>';
-	echo '<p class="last-content-element">' . $error_message . '</p>';
+	if (isset($header))
+	{
+		echo '<h3 class="first-content-element">' . $header . '</h3>';
+		echo '<p class="last-content-element">' . $error_message . '</p>';
+	}
+	else
+	{
+		echo '<p class="first-content-element last-content-element">' . $error_message . '</p>';
+	}
 }
 else
 {
-	echo '<p class="first-content-element last-content-element">' . $error_message . '</p>';
+	show_404();
 }
 ?>
